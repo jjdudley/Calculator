183694b97209adf6ed9f627481623b6fbf1c3293
Josh Dudley <joshdudley.vt@gmail.com>
2020-03-23 01:41:06 -0400

    Update script

--- script.js
+++ script.js
@@ -1,4 +1,126 @@
 class Calculator {
+    constructor (previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    };
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+        this.previousOperandTextElement.innerText = this.previousOperand;
+    };
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    };
+
+    delete () {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    };
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.operate();
+        }
+
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    operate () {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+': 
+            computation = prev + current;
+            break;
+            case '-': 
+            computation = prev - current;
+            break;
+            case '*': 
+            computation = prev * current;
+            break;
+            case '÷': 
+            if (current === 0) {
+            this.clear()
+            this.currentOperand = 'You cannot divide by zero';
+            
+            }
+            else if (current !== 0) {
+            computation = prev / current;
+            }
+            break;
+            default:
+                return;
+        }
+
+        if (Number.isInteger(computation) === false) {
+            computation = (Math.round( computation * 100 + Number.EPSILON ) / 100); 
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
+    updateDisplay () {
+        this.currentOperandTextElement.innerText = this.currentOperand;
+        if (this.operation != null) {
+        this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+        }
+    }
+
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.operate();
+    calculator.updateDisplay();
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
+
+
+
+
+/*class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
@@ -106,14 +228,13 @@ deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
 })
+*/
 
-
-/*
-const operations = {
+/* const operations = {
     "+": add,
     "-": subtract,
     "*": multiply,
-    "/": divide,
+   "/": divide,
 };
 
 
